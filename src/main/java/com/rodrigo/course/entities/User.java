9430ddef73bb86f0b1b77ec4295a8c297325cec4
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-16 13:24:10 -0300

    Order, instant, ISO 8601 PARTE-1

--- src/main/java/com/rodrigo/course/entities/User.java
+++ src/main/java/com/rodrigo/course/entities/User.java
@@ -1,20 +1,23 @@
 package com.rodrigo.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;//COM O NOVO SPRING AGORA É O FINAL JAKARTA
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 //user é a identidade do modelo de dominio
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,74 +25,82 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
-public User() {
-	
-}
 
-public User(Long id, String name, String email, String phone, String password) {
-	super();
-	this.id = id;
-	this.name = name;
-	this.email = email;
-	this.phone = phone;
-	this.password = password;
-}
+	@OneToMany(mappedBy = "client")
+	private List<Order> order = new ArrayList<>();
 
-public Long getId() {
-	return id;
-}
+	public User() {
 
-public void setId(Long id) {
-	this.id = id;
-}
+	}
 
-public String getName() {
-	return name;
-}
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public Long getId() {
+		return id;
+	}
 
-public String getEmail() {
-	return email;
-}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-public void setEmail(String email) {
-	this.email = email;
-}
+	public String getName() {
+		return name;
+	}
 
-public String getPhone() {
-	return phone;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public void setPhone(String phone) {
-	this.phone = phone;
-}
+	public String getEmail() {
+		return email;
+	}
 
-public String getPassword() {
-	return password;
-}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-public void setPassword(String password) {
-	this.password = password;
-}
+	public String getPhone() {
+		return phone;
+	}
 
-@Override
-public int hashCode() {
-	return Objects.hash(id);
-}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	User other = (User) obj;
-	return Objects.equals(id, other.id);
-}
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Order> getOrder() {
+		return order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
 }
