4f002c27222bed16f45fe74df00110ea4b012658
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-16 12:07:26 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/rodrigo/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rodrigo.course.repositories;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+import com.rodrigo.course.entities.User;
+
+
+
+public interface UserRepository extends JpaRepositoryImplementation<User,Long>{
+
+}
