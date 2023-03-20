2d6f6e6eb4c0b63b6d6b24ade3a779e3f999996a
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-20 13:48:55 -0300

    Exception handling - findById

--- src/main/java/com/rodrigo/course/entities/Order.java
+++ src/main/java/com/rodrigo/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rodrigo.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.CascadeType;
@@ -25,6 +26,8 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm>ss'Z'", timezone = "GMT")
 	private Instant moment;
 
 	private Integer orderStatus;
