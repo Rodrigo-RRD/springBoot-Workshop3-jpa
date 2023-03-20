9051fd6cfc7dc73837cc227a81a70a9fd990f389
Rodrigo-RRD <116466164+Rodrigo-RRD@users.noreply.github.com>
2023-03-20 14:11:07 -0300

    Exception handling - delete

--- src/main/java/com/rodrigo/course/services/UserService.java
+++ src/main/java/com/rodrigo/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.rodrigo.course.entities.User;
 import com.rodrigo.course.repositories.UserRepository;
+import com.rodrigo.course.services.exceptions.DatabaseException;
 import com.rodrigo.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -25,19 +28,27 @@ public class UserService {
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
-	public User insert(User obj){
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	public void delete(Long id){
-	repository.deleteById(id);
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e ){
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
-	public User update(Long id, User obj){
+	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
 	}
-	private void updateData(User entity, User obj){
+
+	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
